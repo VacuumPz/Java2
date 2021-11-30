25f8b8187903ad93e9c99094f2875c90176f39bf
VacuumPz <92035639+VacuumPz@users.noreply.github.com>
2021-11-30 10:19:35 +0300

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    public String name;
+    public int run;
+    public int jump;
+
+    Player(String name, int run, int jump) {
+        this.name = name;
+        this.run = run;
+        this.jump = jump;
+    }
+}
