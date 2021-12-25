1f04472a54c4719e0ecca459e10ee8f5889b149d
VacuumPz <92035639+VacuumPz@users.noreply.github.com>
2021-12-26 02:04:26 +0300

    Add files via upload

---
+++ Les8/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, SQLException {
+        System.out.println(Requester.sendCityIdRequest("Moscow"));
+        ReadTable.readTable();
+    }
+}
